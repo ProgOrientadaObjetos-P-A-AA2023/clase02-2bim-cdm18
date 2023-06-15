c4fd6d631e14a72db1e91b4f2005d36142ac340c
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-06-15 17:04:16 -0500

    clase 02 bim/ constructores con herencias y ejercicio

--- EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
+++ EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
@@ -12,35 +12,55 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejecutor2 {
+
     public static void main(String[] args) {
         // Ingresar estudiante de tipo presencial por teclado.
         // El usuario decide cuando terminar
         // No utilizar super.toString en EstudiantePresencial
         // Debe existir 1 sola impresión de todos los estudiantes presenciales
         // No usar arreglos
-        
-        // Trabajo clases 16 junio 2022
+
+        // Trabajo clases 15 junio 2023
         Scanner entrada = new Scanner(System.in);
         boolean bandera = true;
         String cadena = "";
-        
-        while(bandera){
-            System.out.println("ingresa un saludo");
-            String saludo = entrada.nextLine();
-            cadena = String.format("%s%s\n", cadena, saludo);
+
+        while (bandera) {
+            System.out.println("-----------Bienvenido-----------");
+            System.out.println("Ingrese nombres del estudiante");
+            String n = entrada.nextLine();
+            System.out.println("Ingrese apellidos del estudiante");
+            String ap = entrada.nextLine();
+            System.out.println("Ingrese identificación");
+            String ced = entrada.nextLine();
+            System.out.println("Ingrese edad");
+            int edad = entrada.nextInt();
+            System.out.println("Ingrese número de créditos");
+            int creditos = entrada.nextInt();
+            System.out.println("Ingrese costo de créditos");
+            double costo = entrada.nextInt();
+            System.out.println("");
+
+            // 
+            EstudiantePresencial ep = new EstudiantePresencial(n, ap, ced,
+                    edad, costo, creditos);
+            ep.calcularMatriculaPresencial();
+
+            cadena = String.format("%s%s\n", cadena, ep);
+
+            entrada.nextLine();
             System.out.println("Desea salir, pulse S");
+
             String opcion = entrada.nextLine();
-            if(opcion.equals("S")){
+            System.out.println("");
+
+            if (opcion.equals("S") || opcion.equals("s")) {
                 bandera = false;
             }
+
         }
-        
+
         System.out.printf("%s\n", cadena);
-        
-        
+
     }
 }
-
-
-
-
